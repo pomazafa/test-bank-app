019e70791b4b5e6fcd15759f4fdfe851af4a7040
Polina Duben <polina.duben@intexsoft.by>
2023-01-17 16:13:15 +0300

    feat: set up swagger and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Bank')
+    .setDescription('The bank API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
