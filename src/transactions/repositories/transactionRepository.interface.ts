4c2157b26bc242c13c94065866bfd6cace17d237
Polina Duben <polina.duben@intexsoft.by>
2023-01-18 09:43:10 +0300

    feat: create transactions module

--- src/transactions/repositories/transactionRepository.interface.ts
+++ src/transactions/repositories/transactionRepository.interface.ts
@@ -1,6 +1,10 @@
 import { BaseInterfaceRepository, UUID } from '../../common';
 import { TransactionEntity } from '..';
 
+export const TransactionRepositoryInterfaceToken = Symbol(
+  'TransactionRepositoryInterface',
+);
+
 export type SearchOptions = {
   date: Date;
   accountId: UUID;
