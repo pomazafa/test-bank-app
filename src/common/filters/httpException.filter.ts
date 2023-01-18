92b0c61a084a0adab6c976c05dd1e8bf9146e225
Polina Duben <polina.duben@intexsoft.by>
2023-01-18 16:35:15 +0300

    feat: add error logging

--- src/common/filters/httpException.filter.ts
+++ src/common/filters/httpException.filter.ts
@@ -13,6 +13,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    console.log(JSON.stringify(exception));
 
     response.status(status).json({
       statusCode: status,
